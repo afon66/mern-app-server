e7306b70cf641c6b94200adb623930a8d26f901d
Afanasi <narik.fara@mail.ru>
2025-03-27 17:47:02 +0200

    changed routes

--- app.js
+++ app.js
@@ -13,38 +13,18 @@ const app = express();
 const port = process.env.PORT || 3000
 const mongoURI = process.env.MONGO_URI
 
-// app.use(logger('dev'));
-app.use(cors())
+app.use(cors()) 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/api', router)
+app.use('https://mern-app-server-1-xnha.onrender.com/api', router)
 app.use('/uploads', express.static('uploads'))
-// app.use(cookieParser());
-// app.set('view engine', 'jade');
 
 mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected at port ' + port))
   .catch(err => console.error(err));
 
-
 if (!fs.existsSync('uploads')) {
   fs.mkdirSync('uploads')
 }
-
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 export default app
